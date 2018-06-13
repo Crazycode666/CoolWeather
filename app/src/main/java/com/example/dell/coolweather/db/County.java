de27416416e52ee3f60813f6ae9dd868baf89490
xc <xc@gmail.com>
2018-06-13 16:21:40 +0800

    第二次提交。

--- app/src/main/java/com/example/dell/coolweather/db/County.java
+++ app/src/main/java/com/example/dell/coolweather/db/County.java
@@ -1,4 +1,30 @@
 package com.example.dell.coolweather.db;
 
-public class County {
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport{
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getCountyName(){
+        return countyName;
+    }
+    public void setCountyName(String countyName){
+        this.countyName=countyName;
+    }
+    public String getWeatherId(){
+        return weatherId;
+    }
+    public void setWeatherId(String weatherId){
+        this.weatherId=weatherId;
+    }
+
 }
